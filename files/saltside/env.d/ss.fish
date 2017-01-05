a78a1d9e3a75acf489dd29463ad11346ff0fca3c
Adam Hawkins <hi@ahawkins.me>
2017-01-05 12:11:59 +0530

    Integrate saltside with ahawkins/life
    This commit adds some handy functions the "ss" shell function. Here's
    some examples:
    
    ss t foo # Add "foo" to the todo list
    ss t     # print the todo list
    ss notes # Open this week's notes file in $EDITOR

--- files/saltside/env.d/ss.fish
+++ files/saltside/env.d/ss.fish
@@ -1,3 +1,19 @@
-function ss --description "Alias for slashdeploy-workstation"
-	saltside-workstation $argv
+function ss --description "Everything for Saltside"
+	switch $argv[1]
+		case 'notes'
+			set -l year (date '+%Y')
+			set -l week (date '+%v') # 00-53
+			set -l week_number (math "$week+1")
+			set -l notes_file "{{ notes_dir }}/$year-w$week_number.md"
+
+			eval $EDITOR $notes_file
+		case 't'
+			if test (count $argv) -ge 2
+				command todo.sh -d ~/.config/saltside/todo.sh $argv[2..1]
+			else
+				command todo.sh -d ~/.config/saltside/todo.sh ls
+			end
+		case '*'
+			saltside-workstation $argv
+	end
 end
---
+++ files/saltside/todo.sh
@@ -0,0 +1,5 @@
+export TODO_DIR={{ todo_dir }}
+
+export TODO_FILE="${TODO_DIR}/todo.txt"
+export DONE_FILE="${TODO_DIR}/done.txt"
+export REPORT_FILE="${TODO_DIR}/report.txt"
