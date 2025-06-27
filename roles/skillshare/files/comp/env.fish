afad2b757ff87674acce9e0118b54c9351bb326b
Adam Hawkins <adam@hawkins.io>
2025-06-27 13:54:46 -1000

    feat(skillshare): add my utils to path

--- roles/skillshare/files/comp/env.fish
+++ roles/skillshare/files/comp/env.fish
@@ -2,4 +2,8 @@ if not string match $S_SRC_DIR/skillshare/workstation/bin $PATH > /dev/null
   set -x PATH $S_SRC_DIR/skillshare/workstation/bin $PATH
 end
 
+if not string match $S_SRC_DIR/skillshare/ahawkins/bin $PATH > /dev/null
+  set -x PATH $S_SRC_DIR/skillshare/ahawkins/bin $PATH
+end
+
 ev -q skillshare
